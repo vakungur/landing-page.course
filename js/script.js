a90f3455d8db160f05141ac553886ed420639d54
Kungurov Roman <vakungur@gmail.com>
2019-11-17 14:23:52 +0300

    Анимация линий блока прогресса

--- js/script.js
+++ js/script.js
@@ -1,4 +1,6 @@
+
 $(document).ready(function (){
+// Скрипт анимации наведения блока Works
 
     $('.works__btns button, .works__btns a').on('focus', function (e) {
         $(this).parents('.works__element').addClass('works__element--active');
@@ -7,4 +9,31 @@ $(document).ready(function (){
     $('.works__btns button, .works__btns a').on('blur', function (e) {
         $(this).parents('.works__element').removeClass('works__element--active');
     });
+
+// Скрипт анимации линии прогресс блока Progress
+
+    function moveProgessBar(node, nodeLine, tooltip, animationLength = 1500) {
+        const progressElement = $(node);
+        progressElement.each(function (value, item) {
+            $(item).find(nodeLine).animate({
+                width: item.dataset.progressPercent + '%'
+            }, animationLength);
+            $(item).find(tooltip).show(animationLength);
+        });
+    }
+
+    let animate = true;
+
+    $(window).scroll(function () {
+
+        if($('.skills').offset().top <= $(window).scrollTop() + 150) {
+            if(animate) {
+                moveProgessBar('.progress__element', '.progress__line', '.progress__tooltip');
+            }
+            animate = false;
+        }
+    });
 });
+
+
+
