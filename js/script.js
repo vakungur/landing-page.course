fe9ccb7fb2d5b5f8bfd99c5e5d8476d198282a47
Kungurov Roman <vakungur@gmail.com>
2019-11-18 21:24:37 +0300

    Стилизация карусели и подключение плагина OwlCarousel

--- js/script.js
+++ js/script.js
@@ -33,6 +33,30 @@ $(document).ready(function (){
             animate = false;
         }
     });
+
+    $('.carousel').owlCarousel({
+        loop:true,
+        margin: 0,
+        nav:true,
+        dots: false,
+        navText: [],
+        items: 5,
+        responsive:{
+            0:{
+                items:1
+            },
+            480:{
+                items:2
+            },
+            768:{
+                items:3
+            },
+            980:{
+                items:5
+            },
+        }
+    })
+
 });
 
 
